74b87bcb0fa31822eb58af66e41353b160245aaf
Jungik Jang <51084152+cheeseBG@users.noreply.github.com>
2023-04-06 18:27:49 +0900

    modify

--- AMC/runner/train.py
+++ AMC/runner/train.py
@@ -6,8 +6,8 @@ import torch.utils.data as DATA
 from torch.optim import lr_scheduler
 import tqdm
 from runner.utils import get_config, model_selection
-from data.dataset import AMCDataset
-from utils import extract_train_sample
+from data.dataset import AMCTrainDataset
+#from utils import extract_train_sample
 
 
 class Trainer:
@@ -40,9 +40,8 @@ class Trainer:
         if not os.path.exists(self.config["save_path"]):
             os.mkdir(self.config["save_path"])
 
-        train_data = AMCDataset(self.config["dataset_path"], snr_range=self.config["snr_range"])
+        train_data = AMCTrainDataset(self.config["dataset_path"], robust=True, snr_range=self.config["snr_range"])
         train_dataset_size = len(train_data)
-
         train_dataloader = DATA.DataLoader(train_data, batch_size=self.batch_size, shuffle=True)
 
         if self.model_path is not None:
@@ -87,40 +86,40 @@ class Trainer:
             torch.save(self.net.state_dict(), os.path.join(self.config["save_path"], "{}.tar".format(epoch)))
             print("saved at {}".format(os.path.join(self.config["save_path"], "{}.tar".format(epoch))))
 
-    def fs_train(self, model, optimizer, train_x, train_y, n_way, n_support, n_query, max_epoch, epoch_size):
-        """
-        Trains the protonet
-        Args:
-            model
-            optimizer
-            train_x (np.array): dataloader dataframes of training set
-            train_y(np.array): labels of training set
-            n_way (int): number of classes in a classification task
-            n_support (int): number of labeled examples per class in the support set
-            n_query (int): number of labeled examples per class in the query set
-            max_epoch (int): max epochs to train on
-            epoch_size (int): episodes per epoch
-        """
-        # divide the learning rate by 2 at each epoch, as suggested in paper
-        scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1, gamma=0.5, last_epoch=-1)
-        epoch = 0  # epochs done so far
-        stop = False  # status to know when to stop
-
-        while epoch < max_epoch and not stop:
-            running_loss = 0.0
-            running_acc = 0.0
-
-            for episode in tqdm.tnrange(epoch_size, desc="Epoch {:d} train".format(epoch + 1)):
-                sample = extract_train_sample(n_way, n_support, n_query, train_x, train_y)
-                optimizer.zero_grad()
-                loss, output = model.proto_train(sample)
-                running_loss += output['loss']
-                running_acc += output['acc']
-                loss.backward()
-                optimizer.step()
-            epoch_loss = running_loss / epoch_size
-            epoch_acc = running_acc / epoch_size
-            print('Epoch {:d} -- Loss: {:.4f} Acc: {:.4f}'.format(epoch + 1, epoch_loss, epoch_acc))
-
-            epoch += 1
-            scheduler.step()
+    # def fs_train(self, model, optimizer, train_x, train_y, n_way, n_support, n_query, max_epoch, epoch_size):
+    #     """
+    #     Trains the protonet
+    #     Args:
+    #         model
+    #         optimizer
+    #         train_x (np.array): dataloader dataframes of training set
+    #         train_y(np.array): labels of training set
+    #         n_way (int): number of classes in a classification task
+    #         n_support (int): number of labeled examples per class in the support set
+    #         n_query (int): number of labeled examples per class in the query set
+    #         max_epoch (int): max epochs to train on
+    #         epoch_size (int): episodes per epoch
+    #     """
+    #     # divide the learning rate by 2 at each epoch, as suggested in paper
+    #     scheduler = torch.optim.lr_scheduler.StepLR(optimizer, 1, gamma=0.5, last_epoch=-1)
+    #     epoch = 0  # epochs done so far
+    #     stop = False  # status to know when to stop
+    #
+    #     while epoch < max_epoch and not stop:
+    #         running_loss = 0.0
+    #         running_acc = 0.0
+    #
+    #         for episode in tqdm.tnrange(epoch_size, desc="Epoch {:d} train".format(epoch + 1)):
+    #             sample = extract_train_sample(n_way, n_support, n_query, train_x, train_y)
+    #             optimizer.zero_grad()
+    #             loss, output = model.proto_train(sample)
+    #             running_loss += output['loss']
+    #             running_acc += output['acc']
+    #             loss.backward()
+    #             optimizer.step()
+    #         epoch_loss = running_loss / epoch_size
+    #         epoch_acc = running_acc / epoch_size
+    #         print('Epoch {:d} -- Loss: {:.4f} Acc: {:.4f}'.format(epoch + 1, epoch_loss, epoch_acc))
+    #
+    #         epoch += 1
+    #         scheduler.step()
