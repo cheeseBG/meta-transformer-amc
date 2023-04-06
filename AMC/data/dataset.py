e47a2fa9e891c58a1336c91785c2c9ad1957339e
Jungik Jang <51084152+cheeseBG@users.noreply.github.com>
2023-04-06 18:27:49 +0900

    modify

--- AMC/data/dataset.py
+++ AMC/data/dataset.py
@@ -1,5 +1,6 @@
 import torch
 from data.transform import AMCTransform
+from runner.utils import get_config
 import torch.utils.data as data
 import os
 import h5py
@@ -8,15 +9,17 @@ from numpy import argwhere
 import numpy as np
 
 
-class AMCDataset(data.Dataset):
-    def __init__(self, root_path, pre=None, mode=None, snr_range=None):
-        super(AMCDataset, self).__init__()
+class AMCTrainDataset(data.Dataset):
+    def __init__(self, root_path, pre=None, mode=None, robust=False, snr_range=None):
+        super(AMCTrainDataset, self).__init__()
 
         self.root_path = root_path
         self.pre = pre
         self.mode = mode
         self.snr_range = snr_range
         self.transforms = AMCTransform()
+        self.robust = robust
+        self.config = get_config('config.yaml')
 
         self.data = h5py.File(os.path.join(self.root_path, "GOLD_XYZ_OSC.0001_1024.hdf5"), 'r')
         self.class_labels = json.load(open(os.path.join(self.root_path, "classes-fixed.json"), 'r'))
@@ -24,25 +27,116 @@ class AMCDataset(data.Dataset):
         self.iq = self.data['X']
         self.onehot = self.data['Y']
         self.snr = np.squeeze(self.data['Z'])
+        self.num_modulation = 24
+        self.num_sample = int(4096 * self.config['train_proportion'])  # sample per modulation-snr
 
+        # Sampling data in snr boundary
         if self.snr_range is not None:
             snr_mask = (self.snr_range[0] <= self.snr) & (self.snr <= self.snr_range[1])
             self.iq = self.iq[snr_mask]
             self.onehot = self.onehot[snr_mask]
             self.snr = self.snr[snr_mask]
 
+        # Sampling data which are easy modulation
+        if self.mode == 'easy':
+            mod_mask = np.array([int(argwhere(self.onehot[i] == 1)) in self.config['easy_class_indice'] for i in range(len(self.onehot))])
+            self.iq = self.iq[mod_mask]
+            self.onehot = self.onehot[mod_mask]
+            self.snr = self.snr[mod_mask]
+            self.num_modulation = len(self.config['easy_class_indice'])
+
+        # Sampling train data
+        # each modulation-snr has 4096 I/Q samples
+        sampling_mask = []
+        for _ in range(self.num_modulation * len(np.unique(self.snr))):
+            sampling_mask.extend([True for _ in range(self.num_sample)])
+            sampling_mask.extend([False for _ in range(4096-self.num_sample)])
+        sampling_mask = np.array(sampling_mask)
+
+        self.iq = self.iq[sampling_mask]
+        self.onehot = self.onehot[sampling_mask]
+        self.snr = self.snr[sampling_mask]
+
     def __len__(self):
         return self.iq.shape[0]
 
     def __getitem__(self, item):
         label = int(argwhere(self.onehot[item] == 1))
-        # print(label)
-        # print(self.snr[item])
         x = self.iq[item].transpose()
-        revers = np.flip(x.copy(), axis=1)
-        x = np.concatenate((x, revers), axis=0)
 
-        sample = {"data": self.transforms(x), "label": label, "snr": self.snr[item]}  # self.transforms(x)
+        if self.robust is True:
+            revers = np.flip(x.copy(), axis=1)
+            x = np.concatenate((x, revers), axis=0)
+
+            sample = {"data": self.transforms(x), "label": label, "snr": self.snr[item]}  # self.transforms(x)
+        else:
+            sample = {"data": x, "label": label, "snr": self.snr[item]}
 
         return sample
 
+
+class AMCTestDataset(data.Dataset):
+    def __init__(self, root_path, pre=None, mode=None, robust=False, snr_range=None):
+        super(AMCTestDataset, self).__init__()
+
+        self.root_path = root_path
+        self.pre = pre
+        self.mode = mode
+        self.snr_range = snr_range
+        self.transforms = AMCTransform()
+        self.robust = robust
+        self.config = get_config('config.yaml')
+
+        self.data = h5py.File(os.path.join(self.root_path, "GOLD_XYZ_OSC.0001_1024.hdf5"), 'r')
+        self.class_labels = json.load(open(os.path.join(self.root_path, "classes-fixed.json"), 'r'))
+
+        self.iq = self.data['X']
+        self.onehot = self.data['Y']
+        self.snr = np.squeeze(self.data['Z'])
+        self.num_modulation = 24
+        self.num_sample = 4096 - int(4096 * self.config['train_proportion'])  # sample per modulation-snr
+
+        # Sampling data in snr boundary
+        if self.snr_range is not None:
+            snr_mask = (self.snr_range[0] <= self.snr) & (self.snr <= self.snr_range[1])
+            self.iq = self.iq[snr_mask]
+            self.onehot = self.onehot[snr_mask]
+            self.snr = self.snr[snr_mask]
+
+        # Sampling data which are easy modulation
+        if self.mode == 'easy':
+            mod_mask = np.array([int(argwhere(self.onehot[i] == 1)) in self.config['easy_class_indice'] for i in
+                                 range(len(self.onehot))])
+            self.iq = self.iq[mod_mask]
+            self.onehot = self.onehot[mod_mask]
+            self.snr = self.snr[mod_mask]
+            self.num_modulation = len(self.config['easy_class_indice'])
+
+        # Sampling train data
+        # each modulation-snr has 4096 I/Q samples
+        sampling_mask = []
+        for _ in range(self.num_modulation * len(np.unique(self.snr))):
+            sampling_mask.extend([False for _ in range(4096 - self.num_sample)])
+            sampling_mask.extend([True for _ in range(self.num_sample)])
+        sampling_mask = np.array(sampling_mask)
+
+        self.iq = self.iq[sampling_mask]
+        self.onehot = self.onehot[sampling_mask]
+        self.snr = self.snr[sampling_mask]
+
+    def __len__(self):
+        return self.iq.shape[0]
+
+    def __getitem__(self, item):
+        label = int(argwhere(self.onehot[item] == 1))
+        x = self.iq[item].transpose()
+
+        if self.robust is True:
+            revers = np.flip(x.copy(), axis=1)
+            x = np.concatenate((x, revers), axis=0)
+
+            sample = {"data": self.transforms(x), "label": label, "snr": self.snr[item]}  # self.transforms(x)
+        else:
+            sample = {"data": x, "label": label, "snr": self.snr[item]}
+
+        return sample
--- AMC/runner/test.py
+++ AMC/runner/test.py
@@ -7,8 +7,8 @@ from argparse import ArgumentParser
 import tqdm
 import numpy as np
 from runner.utils import get_config, model_selection
-from data.dataset import AMCDataset
-from utils import extract_test_sample
+from data.dataset import AMCTestDataset
+# from utils import extract_test_sample
 
 
 class Tester:
@@ -36,8 +36,7 @@ class Tester:
         self.net.load_state_dict(torch.load(self.model_path))
 
         if not self.per_snr:
-            test_data = AMCDataset(self.config["test_dataset_path"], snr_range=self.config["snr_range"])
-            test_data_size = len(test_data)
+            test_data = AMCTestDataset(self.config["test_dataset_path"], robust=True, snr_range=self.config["snr_range"])
             test_dataloader = DATA.DataLoader(test_data, batch_size=self.batch_size, shuffle=True)
 
             correct = 0
@@ -75,8 +74,7 @@ class Tester:
             f = open(os.path.join(os.path.dirname(self.model_path), "acc.txt"), "w")
 
             for snr in snr_range:
-                test_data = AMCDataset(self.config["test_dataset_path"], snr_range=(snr, snr))
-                test_data_size = len(test_data)
+                test_data = AMCTestDataset(self.config["test_dataset_path"], robust=True, snr_range=(snr, snr))
                 test_dataloader = DATA.DataLoader(test_data, batch_size=self.batch_size, shuffle=True)
 
                 self.net.eval()
@@ -103,54 +101,54 @@ class Tester:
             f.close()
 
     #Todo
-    def fs_test(self, model, test_x, test_y, n_way, n_support, n_query, test_episode):
-        """
-        Tests the protonet
-        Args:
-            model: trained model
-            test_x (np.array): dataloader dataframes of testing set
-            test_y (np.array): labels of testing set
-            n_way (int): number of classes in a classification task
-            n_support (int): number of labeled examples per class in the support set
-            n_query (int): number of labeled examples per class in the query set
-            test_episode (int): number of episodes to test on
-        """
-        print("Cuda: ", torch.cuda.is_available())
-        print("Device id: ", self.device_ids[0])
-
-        conf_mat = torch.zeros(n_way, n_way)
-        running_loss = 0.0
-        running_acc = 0.0
-
-        '''
-        Modified
-        # Extract sample just once
-        '''
-        sample = extract_test_sample(n_way, n_support, n_query, test_x, test_y)
-        query_samples = sample['query']
-
-        # Create target domain Prototype Network with support set(target domain)
-        z_proto = model.create_protoNet(sample)
-
-        total_count = 0
-        model.eval()
-        with torch.no_grad():
-            for label, sample in enumerate(tqdm.tqdm(query_samples)):
-                for i in range(0, len(sample) // n_way):
-                    if self.use_cuda:
-                        x = sample.to(self.device_ids[0])
-                    else:
-                        x = sample
-
-                    output = model.proto_test(x[i * n_way:(i + 1) * n_way], z_proto, n_way, label)
-                    a = output['y_hat'].cpu().int()
-                    for cls in range(n_way):
-                        conf_mat[cls, :] = conf_mat[cls, :] + torch.bincount(a[cls, :], minlength=n_way)
-
-                    running_acc += output['acc']
-                    total_count += 1
-
-        avg_acc = running_acc / total_count
-        print('Test results -- Acc: {:.4f}'.format(avg_acc))
-        return (conf_mat / (test_episode * n_query), avg_acc)
+    # def fs_test(self, model, test_x, test_y, n_way, n_support, n_query, test_episode):
+    #     """
+    #     Tests the protonet
+    #     Args:
+    #         model: trained model
+    #         test_x (np.array): dataloader dataframes of testing set
+    #         test_y (np.array): labels of testing set
+    #         n_way (int): number of classes in a classification task
+    #         n_support (int): number of labeled examples per class in the support set
+    #         n_query (int): number of labeled examples per class in the query set
+    #         test_episode (int): number of episodes to test on
+    #     """
+    #     print("Cuda: ", torch.cuda.is_available())
+    #     print("Device id: ", self.device_ids[0])
+    #
+    #     conf_mat = torch.zeros(n_way, n_way)
+    #     running_loss = 0.0
+    #     running_acc = 0.0
+    #
+    #     '''
+    #     Modified
+    #     # Extract sample just once
+    #     '''
+    #     sample = extract_test_sample(n_way, n_support, n_query, test_x, test_y)
+    #     query_samples = sample['query']
+    #
+    #     # Create target domain Prototype Network with support set(target domain)
+    #     z_proto = model.create_protoNet(sample)
+    #
+    #     total_count = 0
+    #     model.eval()
+    #     with torch.no_grad():
+    #         for label, sample in enumerate(tqdm.tqdm(query_samples)):
+    #             for i in range(0, len(sample) // n_way):
+    #                 if self.use_cuda:
+    #                     x = sample.to(self.device_ids[0])
+    #                 else:
+    #                     x = sample
+    #
+    #                 output = model.proto_test(x[i * n_way:(i + 1) * n_way], z_proto, n_way, label)
+    #                 a = output['y_hat'].cpu().int()
+    #                 for cls in range(n_way):
+    #                     conf_mat[cls, :] = conf_mat[cls, :] + torch.bincount(a[cls, :], minlength=n_way)
+    #
+    #                 running_acc += output['acc']
+    #                 total_count += 1
+    #
+    #     avg_acc = running_acc / total_count
+    #     print('Test results -- Acc: {:.4f}'.format(avg_acc))
+    #     return (conf_mat / (test_episode * n_query), avg_acc)
 
