f19426b4ebaf1eea216999db8e8f6f7810edb438
Jungik Jang <51084152+cheeseBG@users.noreply.github.com>
2023-04-10 19:29:51 +0900

    update readme

--- AMC/main.py
+++ AMC/main.py
@@ -1,4 +1,5 @@
 import logging
+import argparse
 from runner.train import Trainer
 from runner.test import Tester
 from runner.utils import CustomFormatter
@@ -11,21 +12,29 @@ if __name__ == '__main__':
     handler.setFormatter(CustomFormatter())
     logger.addHandler(handler)
 
+    parser = argparse.ArgumentParser(description='Process some integers.')
+    parser.add_argument('lr_mode', type=str, default='fs', help='Select learning method: sv(supervised), fs(few-shot)')
+
+    args = parser.parse_args()
+
     # Supervised learning
-    # logger.info('Start supervised learning')
-    # trainer = Trainer("config.yaml")
-    # trainer.train()
-    # tester = Tester("config.yaml", per_snr=True)
-    # tester.test()
+    if args.lr_mode == 'sv':
+        logger.info('Start supervised learning')
+        trainer = Trainer("config.yaml")
+        trainer.train()
+        tester = Tester("config.yaml", per_snr=True)
+        tester.test()
+    elif args.lr_mode == 'fs':
+        # Few shot learning
+        logger.info('Start few-shot learning')
+        trainer = Trainer("config.yaml")
+        trainer.fs_train()
 
-    # Few shot learning
-    # logger.info('Start few-shot learning')
-    # trainer = Trainer("config.yaml")
-    # trainer.fs_train()
-
-    # tester = Tester("config.yaml")
-    # logger.info('Original Test')
-    # tester.fs_test()
-    # logger.info('New Metric Test ')
-    # tester.fs_test_once()
+        tester = Tester("config.yaml")
+        logger.info('Original Test')
+        tester.fs_test()
+        logger.info('New Metric Test ')
+        tester.fs_test_once()
+    else:
+        logger.error('Wrong argument!')
 
