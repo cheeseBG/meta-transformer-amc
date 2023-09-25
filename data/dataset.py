5c03a3b40f2f1843289b4bedf8f400f05155a30e
Jungik Jang <jji940429@gmail.com>
2023-09-25 17:28:07 +0900

    add RML2016 dataset

--- data/dataset.py
+++ data/dataset.py
@@ -1,6 +1,7 @@
 import os
 import h5py
 import json
+import pickle
 import numpy as np
 import random
 import torch
@@ -320,3 +321,172 @@ class FewShotDatasetForOnce(data.Dataset):
                 sample[label]['query'] = query_set
 
         return sample
+    
+
+'''
+RML2016.10A
+'''
+class FewShotDataset2016(data.Dataset):
+    def __init__(self, root_path, num_support, num_query, mode='train', robust=False, extension=False, snr_range=None, divide=False, sample_len=128):
+        self.root_path = root_path
+        self.robust = robust
+        self.extension = extension
+        self.snr_range = snr_range
+        self.mode = mode
+        self.config = get_config('config.yaml')
+        self.num_sample = int(1000 * self.config['train_proportion'])  # sample
+        self.divide = divide
+        self.labels = list()
+        if mode == 'train':
+            self.labels = [self.config['total_class16'][idx] for idx in self.config['train_class_indice16']]
+        elif mode == 'test':
+           self.labels = [self.config['total_class16'][idx] for idx in self.config['test_class_indice16']]
+        self.num_modulation = len(self.labels)
+
+        with open(self.root_path, 'rb') as f:
+            u = pickle._Unpickler(f)
+            u.encoding = 'latin1'
+            data_dict = u.load()
+
+        self.data = data_dict
+        self.sample_len = sample_len
+        #self.train_sample_len = self.config['train_sample_size']
+        self.train_sample_len = 128
+
+        self.iq = None
+        self.onehot = None
+        # Sampling data in snr boundary
+        for mod_idx, mod in enumerate(self.labels):
+            for snr in range(self.snr_range[0], self.snr_range[1], 2):
+                onehot = np.zeros(self.num_modulation)
+                onehot[mod_idx] = 1
+
+                if self.iq != None:
+                    if self.divide is True:
+                        if mode == 'train':
+                            self.iq.vstack((self.iq, self.data[(mod, snr)[:self.num_sample]]))
+                            onehot_arr = np.array([onehot for _ in range(self.num_sample)])
+                            self.onehot.vstack((self.onehot, onehot_arr))
+                        elif mode == 'test':
+                            self.iq.vstack((self.iq, self.data[(mod, snr)[self.num_sample:]]))
+                            onehot_arr = np.array([onehot for _ in range(1000-self.num_sample)])
+                            self.onehot.vstack((self.onehot, onehot_arr))
+                    else:
+                        self.iq.vstack((self.iq, self.data[(mod, snr)]))
+                        onehot_arr = np.array([onehot for _ in range(1000)])
+                        self.onehot.vstack((self.onehot, onehot_arr))
+                else:
+                    if self.divide is True:
+                        if mode == 'train':
+                            self.iq = self.data[(mod, snr)[:self.num_sample]]
+                            self.onehot = np.array([onehot for _ in range(self.num_sample)])
+                        elif mode == 'test':
+                            self.iq = self.data[(mod, snr)[self.num_sample:]]
+                            self.onehot = np.array([onehot for _ in range(1000-self.num_sample)])
+                    else:
+                        self.iq = self.data[(mod, snr)]
+                        self.onehot = np.array([onehot for _ in range(1000)])
+  
+        # for extension
+        if self.extension is True:
+            print("Create amp & phase...")
+            self.cmp = self.iq[..., 0] + 1j * self.iq[..., 1]
+            self.exts = np.expand_dims(np.abs(self.cmp), axis=1)
+            #self.phase = np.expand_dims(np.angle(self.cmp), axis=1)
+            #self.exts = np.concatenate([self.amp, self.phase], axis=1)
+            print("Done")
+
+        # Extract indice of each labels
+        self.label_indices = {label: [i for i, x in enumerate(self.label_list) if x == label] for label in self.labels}
+
+        # few-shot variables
+        self.num_support = num_support
+        self.num_query = num_query
+        self.num_episode = len(self.iq) // ((self.num_support + self.num_query) * len(self.labels))
+
+    def __len__(self):
+        return self.num_episode
+
+    def __getitem__(self, idx):
+        # idx means index of episode
+        sample = dict()
+
+        if self.robust is True:
+            for label in self.labels:
+                sample[label] = dict()
+                label_indices = self.label_indices[label]
+
+                # support set
+                support_indices = random.sample(label_indices, self.num_support)
+                # support_set = [np.concatenate((self.iq[i].transpose()[:, :self.sample_len],
+                #                                np.flip(self.iq[i].transpose()[:, :self.sample_len], axis=1)), axis=0)
+                #                for i in support_indices]
+                support_set = [np.concatenate((self.iq[i][:, :self.sample_len],
+                                               self.iq[i][:, :self.sample_len]), axis=0)
+                               for i in support_indices]
+                sample[label]['support'] = support_set
+
+                # query set
+                query_indices = list(set(label_indices) - set(support_indices))
+                query_indices = random.sample(query_indices, self.num_query)
+                # query_set = [np.concatenate((self.iq[i].transpose(), np.flip(self.iq[i].transpose(), axis=1)), axis=0)
+                #              for i in query_indices]
+                query_set = [np.concatenate((self.iq[i], self.iq[i]), axis=0) for i in query_indices]
+                sample[label]['query'] = query_set
+
+        elif self.extension is True:
+            for label in self.labels:
+                sample[label] = dict()
+                label_indices = self.label_indices[label]
+
+                # support set
+                support_indices = random.sample(label_indices, self.num_support)
+                support_set = None
+                support_set = [np.concatenate((self.iq[i][:, :self.train_sample_len],
+                                            self.exts[i][:, :self.train_sample_len]), axis=0)
+                            for i in support_indices]
+                sample[label]['support'] = support_set
+
+                # query set
+                query_indices = list(set(label_indices) - set(support_indices))
+                query_indices = random.sample(query_indices, self.num_query)
+                query_set = None
+                if self.mode == 'train':
+                    query_set = [np.concatenate((self.iq[i][:, :self.train_sample_len],
+                                                self.exts[i][:, :self.train_sample_len]), axis=0) for i in query_indices]
+                else:
+                    # query_set = [np.concatenate((self.iq[i].transpose()[:, :self.sample_len],
+                    #                            self.exts[i][:, :self.sample_len]), axis=0) for i in query_indices]
+                     # self-duplicate
+                    num_dup = (self.train_sample_len // self.sample_len)
+                    query_set = [np.concatenate([np.concatenate((self.iq[i][:, :self.sample_len],
+                                               self.exts[i][:, :self.sample_len]), axis=0) for _ in range(num_dup)], axis=1) for i in query_indices]
+                sample[label]['query'] = query_set
+
+        else:
+            for label in self.labels:
+                sample[label] = dict()
+                label_indices = self.label_indices[label]
+
+                # support set
+                support_indices = random.sample(label_indices, self.num_support)
+                support_set = [self.iq[i][:, :self.train_sample_len] for i in support_indices]
+                sample[label]['support'] = support_set
+
+                # query set
+                query_indices = list(set(label_indices) - set(support_indices))
+                query_indices = random.sample(query_indices, self.num_query)
+                query_set = None
+                if self.mode == 'train':
+                    query_set = [self.iq[i][:, :self.train_sample_len] for i in query_indices]
+                else:
+                    # zero padding
+                    query_set = [np.concatenate((self.iq[i][:, :self.sample_len], 
+                                                 np.zeros((2, self.train_sample_len-self.sample_len),dtype=np.float32)), axis=1) for i in query_indices]
+                                                 
+                    # self-duplicate
+                    num_dup = (self.train_sample_len // self.sample_len)
+                    query_set = [np.concatenate([self.iq[i][:, :self.sample_len] for _ in range(num_dup)], axis=1) for i in query_indices]
+                sample[label]['query'] = query_set
+        return sample
+    
